9b809406b541e32ebd892a8807d76238cde25b20
Eduardo <edu.vlemes@gmail.com>
2025-10-08 10:06:03 -0300

    .

--- template/js/netlify-cms/base-config/index.js
+++ template/js/netlify-cms/base-config/index.js
@@ -17,7 +17,7 @@ export default options => {
       widget: 'select',
           multiple: true,
           options: options.state.routes
-              .filter(({ sku }) => typeof sku != 'undefined')
+              .filter(({ sku }) => typeof sku === 'string')
               .map(({ _id, sku }) => ({
                 label: sku,
                 value: _id 
